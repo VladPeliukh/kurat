b420b532b91aeeee5172e922d253fb821fe9e8fb
Mitya <148676835+dmitruez@users.noreply.github.com>
2025-11-15 16:46:49 +0300

    Revise curator menu actions and command setup

--- telegram/bot/keyboards/__init__.py
+++ telegram/bot/keyboards/__init__.py
@@ -1,17 +1,19 @@
 from .captcha_keyboards import captcha_options_keyboard
 from .curator_keyboards import (
+    curator_cancel_message_keyboard,
     curator_main_menu_keyboard,
+    curator_invite_keyboard,
     curator_partners_keyboard,
-    curator_partners_stats_keyboard,
     curator_request_keyboard,
     format_partner_title,
 )
 
 __all__ = [
     "captcha_options_keyboard",
+    "curator_cancel_message_keyboard",
     "curator_main_menu_keyboard",
+    "curator_invite_keyboard",
     "curator_partners_keyboard",
-    "curator_partners_stats_keyboard",
     "curator_request_keyboard",
     "format_partner_title",
 ]
